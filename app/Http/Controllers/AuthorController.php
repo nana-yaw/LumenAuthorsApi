681e3a83910e41d289fbcf1a8b25103b68dd7a1f
nana-yaw <edward.osei.nyarko@gmail.com>
2020-01-31 13:14:44 +0000

    Remove existing author controller method added

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -95,7 +95,11 @@ class AuthorController extends Controller
      */
     public function destroy($author)
     {
+        $author = Author::findOrFail($author);
 
+        $author->delete();
+
+        return $this->successResponse($author);
     }
 
     //
